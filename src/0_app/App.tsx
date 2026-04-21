073333dac148cffc6da61fc948c2d6a209114be3
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-04-21 13:35:24 +0300

    init app

---
+++ src/0_app/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@shared/ui/shadcn/button";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1 className="text-4xl">Hi</h1>
+                <Button>Click</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
